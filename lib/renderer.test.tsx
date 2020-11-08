a1cd83bbde0c3c9dcd3ee1310c6a444af6d0dcf5
clomie <cclomie@gmail.com>
2020-11-08 21:18:08 +0900

    Add tests for components, dangerouslySetInnerHTML, Fragments

--- lib/renderer.test.tsx
+++ lib/renderer.test.tsx
@@ -1,4 +1,4 @@
-import { h } from './vnode'
+import { ChildNodes, Component, Fragment, h } from './vnode'
 import { render } from './render'
 
 describe('render', () => {
@@ -162,6 +162,164 @@ describe('render', () => {
         const expected = `<${element} foo="bar">baz</${element}>`
         expect(rendered).toEqual(expected)
       })
+    })
+  })
+
+  describe('Functional Components', () => {
+    it('should render functional components', () => {
+      const Test = jest.fn(
+        ({ foo, children }: { foo: string; children?: ChildNodes }) => (
+          <div foo={foo}>{children}</div>
+        )
+      )
+      const rendered = render(<Test foo="test">content</Test>)
+
+      expect(rendered).toEqual(`<div foo="test">content</div>`)
+      expect(Test).toHaveBeenCalledTimes(1)
+      expect(Test).toHaveBeenCalledWith({ foo: 'test', children: ['content'] })
+    })
+
+    it('should render functional components within JSX', () => {
+      const Test = jest.fn(
+        ({ foo, children }: { foo: number; children?: ChildNodes }) => (
+          <div foo={foo}>{children}</div>
+        )
+      )
+
+      const rendered = render(
+        <section>
+          <Test foo={1}>
+            <span>bar</span>
+            <span>baz</span>
+          </Test>
+        </section>
+      )
+      const expected = `<section><div foo="1"><span>bar</span><span>baz</span></div></section>`
+
+      expect(rendered).toEqual(expected)
+      expect(Test).toHaveBeenCalledTimes(1)
+      expect(Test).toHaveBeenCalledWith({
+        foo: 1,
+        children: [
+          { type: 'span', attributes: {}, children: ['bar'] },
+          { type: 'span', attributes: {}, children: ['baz'] },
+        ],
+      })
+    })
+
+    it('should render high order components', () => {
+      const Inner: Component = ({ children, ...props }) => (
+        <div id="inner" {...props} b="c" c="d">
+          {children}
+        </div>
+      )
+      const Outer: Component = ({ children, ...props }) => (
+        <Inner {...props} a="b">
+          child <span>{children}</span>
+        </Inner>
+      )
+      const rendered = render(
+        <Outer a="a" b="b" p={1}>
+          foo
+        </Outer>
+      )
+      const expected =
+        '<div id="inner" a="b" b="c" p="1" c="d">child <span>foo</span></div>'
+      expect(rendered).toEqual(expected)
+    })
+  })
+
+  describe('dangerouslySetInnerHTML', () => {
+    it('should support dangerouslySetInnerHTML', () => {
+      const rawHtml = '<a foo="bar">baz</a> foo & bar <d><e>foo<e>bar</d>'
+      const rendered = render(
+        <div foo="bar" dangerouslySetInnerHTML={{ __html: rawHtml }} />
+      )
+      expect(rendered).toEqual(`<div foo="bar">${rawHtml}</div>`)
+    })
+
+    it('should override children', () => {
+      let rendered = render(
+        <div dangerouslySetInnerHTML={{ __html: 'foo' }}>
+          <b>bar</b>
+        </div>
+      )
+      expect(rendered).toEqual('<div>foo</div>')
+    })
+  })
+
+  describe('Fragments', () => {
+    it('should skip Fragment node', () => {
+      let html = render(
+        <div>
+          <Fragment>foo</Fragment>
+        </div>
+      )
+      expect(html).toEqual('<div>foo</div>')
+    })
+
+    it('should skip Fragment node with text and element', () => {
+      let html = render(
+        <div>
+          <Fragment>
+            foo
+            <span>bar</span>
+          </Fragment>
+        </div>
+      )
+      expect(html).toEqual('<div>foo<span>bar</span></div>')
+    })
+
+    it('should skip Fragment node with multiple elements', () => {
+      let html = render(
+        <div>
+          <Fragment>
+            <div>foo</div>
+            <div>bar</div>
+          </Fragment>
+        </div>
+      )
+      expect(html).toEqual('<div><div>foo</div><div>bar</div></div>')
+    })
+
+    it('should skip Fragment with props', () => {
+      let html = render(
+        <div>
+          <Fragment foo="bar">foo</Fragment>
+        </div>
+      )
+      expect(html).toEqual('<div>foo</div>')
+    })
+
+    it('should skip sibling Fragments', () => {
+      let html = render(
+        <div>
+          <Fragment>foo</Fragment>
+          <Fragment>bar</Fragment>
+        </div>
+      )
+      expect(html).toEqual('<div>foobar</div>')
+    })
+
+    it('should skip nested Fragments', () => {
+      let html = render(
+        <div>
+          <Fragment>
+            <Fragment>foo</Fragment>
+          </Fragment>
+        </div>
+      )
+      expect(html).toEqual('<div>foo</div>')
+    })
+
+    it('should skip Fragment as root node', () => {
+      let html = render(
+        <Fragment>
+          <span>foo</span>
+          <span>bar</span>
+        </Fragment>
+      )
+      expect(html).toEqual('<span>foo</span><span>bar</span>')
     })
   })
 })
