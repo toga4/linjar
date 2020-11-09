dfdb372d6e4e64e7dc99e0ee3f8788d4c8fa6707
clomie <cclomie@gmail.com>
2020-11-10 00:10:38 +0900

    Move declaration of JSX.IntrinsicElements

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
 export { render } from './render'
 export { ChildNodes, Component, Fragment, h, VNode } from './vnode'
 
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      [elemName: string]: unknown
+    }
+  }
+}
--- src/renderer.test.tsx
+++ src/renderer.test.tsx
@@ -1,14 +1,6 @@
 import { ChildNodes, Component, Fragment, h } from './vnode'
 import { render } from './render'
 
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      [elemName: string]: unknown
-    }
-  }
-}
-
 describe('render', () => {
   describe('Basic JSX', () => {
     it('should render JSX', () => {
